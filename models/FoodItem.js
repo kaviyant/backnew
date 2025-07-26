a10129486004639ecc6a48f119242a63f1ce2950
KAVIYAN T <67645078+kaviyant@users.noreply.github.com>
2025-07-27 00:49:23 +0530

    Update FoodItem.js

--- models/FoodItem.js
+++ models/FoodItem.js
@@ -1,11 +1,10 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
-const foodItemSchema = new mongoose.Schema({
+const FoodItemSchema = new mongoose.Schema({
   name: String,
   price: Number,
   count: Number,
-  available: { type: Boolean, default: true },
   shopID: String
 });
 
-module.exports = mongoose.model('FoodItem', foodItemSchema);
+module.exports = mongoose.model("FoodItem", FoodItemSchema);
